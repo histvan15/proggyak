7912cf8ff74ba6f22123055518aba06701d53ff9
histvan10 <histvan1510@gmail.com>
2021-09-28 16:26:28 +0200

    asd

---
+++ 04 het 03 feladat/main.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int szam, i=2, db=0, talalt=0;
+
+    printf("Kerek egy szamot: "); scanf("%d", &szam);
+
+    /*for (i=2; i<=szam/2; i++)
+
+        if (szam%i==0)
+
+            db += 1;
+
+            if (db)
+
+                printf("A megadott szam nem prim.");
+
+            else
+
+                printf("A megadott szam prim.");
+*/
+/*
+    while (i<=szam/2 && !talalt) {
+
+        if (szam%i)
+
+            { i++; continue; }
+
+        else
+
+            talalt=1;
+
+    }
+
+        if (talalt)
+
+            printf("A megadott szam nem prim.");
+
+        else
+
+            printf("A megadott szam prim.");
+*/
+
+    for (i=2; i<=szam/2; i++) {
+
+        if (szam%i)
+
+            continue;
+
+        else{
+
+            talalt=1;
+            break;
+        }
+    }
+        if (talalt)
+
+            printf("A megadott szam nem prim.");
+
+        else
+
+            printf("A megadott szam prim.");
+
+    return 0;
+}
